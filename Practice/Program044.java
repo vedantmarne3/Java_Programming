daaffa500a435ff4dfd4db7465f1be47359bdcb8
vedantmarne3 <vedantbmarne@gmail.com>
2026-06-23 22:16:24 +0530

    Renamed files

---
+++ Practice/Program044.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class  program142
+{
+    public static void main(String[] A) 
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iCnt = 0;
+
+
+       System.out.println("Enter the No. of Elements : ");
+       int iLength = sobj.nextInt();
+
+        int Brr[] = new int[iLength];
+
+        System.out.println("Enter the Element : ");
+        for(iCnt = 0; iCnt < Brr.length; iCnt++)
+        {
+            Brr[iCnt] = sobj.nextInt();
+        }
+
+        System.out.println("Elements of Array are : ");
+        for(iCnt = 0; iCnt < Brr.length; iCnt++)
+        {
+            System.out.println(Brr[iCnt]);
+        }
+
+        Brr = null;
+
+        System.gc();
+    }
+}
