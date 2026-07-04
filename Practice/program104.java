412441666a7c6945147b8aa494266a285674ce19
vedantmarne3 <vedantbmarne@gmail.com>
2026-07-04 20:17:51 +0530

    Added new programs

---
+++ Practice/program104.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+class program225
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue1 = 0, iValue2;
+
+        Pattern pobj = new Pattern();
+
+        System.out.println("Enter no. of Rows : ");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter no. of Column : ");
+        iValue2 = sobj.nextInt();
+
+
+        pobj.Display(iValue1, iValue2);
+
+    }
+}
+
+class Pattern
+{
+    public void Display(int iRow, int iCol)
+    {
+        int i = 0, j = 0;
+        char ch = '\0';
+
+
+        for(i = 1; i <= iRow; i++)
+        {
+            
+            for(j = 1,ch = 'a'; j <= i; j++,ch++)
+            {
+
+                System.out.print(ch+"\t");
+                
+            }
+            System.out.println();
+        }
+
+    }
+}
+/*
+    iRow = 4
+    iCol = 4
+
+    a
+    ab
+    abc
+    abcd
+
+
+*/
