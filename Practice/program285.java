d8f13cd4d97c0a854b44bc010c0f98d0c476cfd1
vedantmarne3 <vedantbmarne@gmail.com>
2026-07-09 23:43:46 +0530

    Added new programs

---
+++ Practice/program285.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+class program285
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        String data = null;
+        StringX strobj = new StringX();
+        String sRet = null;
+
+        System.out.println("Enter String : ");
+        data = sobj.nextLine();
+
+
+        sRet = strobj.toUpperX(data);
+
+        System.out.println("Updated String is : "+sRet);
+
+
+
+    }
+}
+
+class StringX
+{
+    public String toUpperX(String str)
+    {
+        int i = 0;
+
+        char Arr[] = str.toCharArray();
+
+        for(i = 0; i < Arr.length; i++)
+        {
+            Arr[i] = (char)(Arr[i] - 32);  // issue capital cha
+        }
+
+        
+
+        return new String(Arr); // return directly
+    }
+
+}
